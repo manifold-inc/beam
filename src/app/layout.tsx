623950f04cb3ad686e9fc325163cd8f7c4b46db6
josh <josh@mjbrowns.com>
2024-07-12 12:03:56 -0500

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import { ThemeProvider } from 'next-themes'
 import 'styles/globals.css'
 import { PropsWithChildren } from 'react'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Beam",
+  description: "Manifolds internal blog",
+};
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
